078f7c0f0e142027ceffe2d03be9bdc2796a85da
ivan-ontruck <ivan@ontruck.com>
2021-12-23 19:51:45 +0100

    Working POC

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,239 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from 'axios';
+import logo from '../public/logo.png'
+import { useEffect, useState } from 'react'
+import { 
+  Connection, 
+  clusterApiUrl,
+  PublicKey,
+} from '@solana/web3.js';
+import { 
+  getParsedNftAccountsByOwner
+} from "@nfteyez/sol-rayz";
 
 export default function Home() {
+
+  const THE_JUNGLE_MINT_ADDRESS = 'H7HeDDfgQEs9Uc3xycb3xTHVHCchf7WA44WWRkREiNHe';
+
+  const [connection, setConnection] = useState('undefined');
+  const [walletInitialized, setwalletInitialized] = useState(false);
+  const [provider, setProvider] = useState('undefined');
+  const [waiting, setWaiting] = useState(0);
+  const [image, setImage] = useState('undefined');
+
+  async function connectButton() {
+      const getProviderPhantom = async () => {
+          if ("solana" in window) {
+              const provider = window.solana;
+              if (provider.isPhantom) {
+                  console.log("Is Phantom installed?  ", provider.isPhantom);
+                  return provider;
+              }
+          } else {
+              window.open("https://www.phantom.app/", "_blank");
+          }
+      };
+
+      let which = 'phantom';
+
+      connection = new Connection(
+        clusterApiUrl('mainnet-beta')
+      );
+
+      setConnection(connection);
+
+      if (!walletInitialized) {
+          console.log("initializing " + which);
+          if (which == 'phantom') {
+              provider = await getProviderPhantom();
+          } else if (which == 'solflare') {
+              provider = await getProviderSolflare();
+          }
+          if (!provider) {
+            return;
+          }
+          provider.connect();
+
+          provider.on("connect", async () => {              
+              setProvider(provider);
+              await getBalance();
+              setwalletInitialized(true);
+          })
+          
+      } else {
+          console.log('already initialized wallet');
+          await getBalance();
+          setwalletInitialized(true);
+      }
+  }
+
+  const getAllNftData = async () => {
+    try {
+        let ownerToken = provider.publicKey;
+        const nfts = await getParsedNftAccountsByOwner({
+          publicAddress: ownerToken,
+          connection: connection,
+          serialization: true,
+        });
+        return nfts;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getNftTokenData = async () => {
+    try {
+      let nftData = await getAllNftData();
+      var data = Object.keys(nftData).map((key) => nftData[key]);                                                                    let arr = [];
+      let n = data.length;      
+      let animal_image;
+      for (let i = 0; i < n; i++) {
+        console.log(data[i].mint);
+        if (data[i].mint == THE_JUNGLE_MINT_ADDRESS && data[i].data) {
+          console.log(data[i].data.uri);
+          //let val = await axios.get(data[i].data.uri);
+          //arr.push(val);  
+          animal_image = await axios.get(data[i].data.uri);
+          console.log(arr[0].data.image);
+          setImage(animal_image);
+          break;
+        }
+      }
+      if (!animal_image) {
+        setImage('no_jungle');
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async function getBalance() {    
+    if (connection && provider) {
+      console.log(provider);
+      setWaiting(1);           
+      let res = await getNftTokenData();
+      setWaiting(0);   
+      console.log(res);  
+      //setBalance(balance);
+    }
+  }
+
+  function renderImage() {
+    if (image != 'undefined' && image != 'no_jungle'){
+      return(
+        <div>
+        <div className={styles.window}>
+        <div id={styles.christmas} style={{ background: {image} }}>
+        <Image  
+            src={image}
+            width={320}
+            height={320}
+        />
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-1"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-2"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-3"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-4"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-5"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-6"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-7"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-8"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-9"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-10"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-11"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-12"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-13"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-14"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-15"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-16"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-17"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-18"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-19"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-20"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-21"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-22"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-23"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-24"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-25"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-26"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-27"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-28"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-29"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-30"]}`}></div>
+        <div className={`${styles["flake"]} ${styles["large"]} ${styles["f-31"]}`}></div>
+        <div className={styles.ground}></div>
+
+        </div>
+
+        
+        </div>
+        
+        </div>
+
+      );
+    } else if (image != 'no_jungle') {
+      return(
+      <div className={styles.button}>
+      <a onClick={connectButton}>Connect Phantom</a>
+      </div>
+    
+      )
+    } else {
+      return(
+      <div>
+      <p className={styles.description}>No animal found on the selected wallet!</p>
+      <div className={styles.button}>
+      <a
+          href="https://magiceden.io/marketplace/the_jungle"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Adopt an animal!</a>
+      </div></div>  )
+    }
+  }
+
+  function renderGreeting() {
+    if (image != 'undefined'){
+      return(
+        <div>
+          <h1 className={styles.title}>Merry Christmas!</h1>
+        </div>
+      );
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>The Jungle Merry Christmas</title>
+        <meta name="description" content="The Jungle Merry Christmas Gretting - Unofficial" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className={styles.main}>        
+        <Image 
+            className="hero-image"
+            src={logo}            
+            quality={100}
+            width={334}
+            height={139}
+            layout="intrinsic"
+          /> 
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        {renderImage()}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {renderGreeting()}        
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://twitter.com/mart_mc_fly"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          <p>Unofficial web3 Christmas greeting, with ♥ by <b>@mart_mc_fly</b></p>
         </a>
       </footer>
     </div>
